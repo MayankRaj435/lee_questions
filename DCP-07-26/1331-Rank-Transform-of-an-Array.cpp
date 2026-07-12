bc28f65a28809730dcd4b98c263e588b780ed13c
Mayank Raj <144890239+MayankRaj435@users.noreply.github.com>
2026-07-12 09:34:18 +0530

    [1331] [Time Beats: 88.64%] [Memory Beats: 71.43%] - LeetPush

---
+++ DCP-07-26/1331-Rank-Transform-of-an-Array.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> arrayRankTransform(vector<int>& arr) {
+
+        vector<int> temp = arr;
+
+        sort(temp.begin(), temp.end());
+
+        unordered_map<int, int> rank;
+
+        int currRank = 1;
+
+        for (int num : temp) {
+
+            if (rank.find(num) == rank.end()) {
+                rank[num] = currRank;
+                currRank++;
+            }
+        }
+
+        for (int i = 0; i < arr.size(); i++) {
+            arr[i] = rank[arr[i]];
+        }
+
+        return arr;
+    }
+};
