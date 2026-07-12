41b5c08b565a5f127b61399f8b31f438739d3c3c
Mayank Raj <144890239+MayankRaj435@users.noreply.github.com>
2026-07-12 12:40:48 +0530

    [23] [Time Beats: 100.00%] [Memory Beats: 38.55%] - LeetPush

--- 23-Merge-k-Sorted-Lists.cpp
+++ 23-Merge-k-Sorted-Lists.cpp
@@ -12,29 +12,29 @@ class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
 
-        vector<int> nums;
-
-        for (int i = 0; i < lists.size(); i++) {
-
-            ListNode* temp = lists[i];
-
-            while (temp != NULL) {
-                nums.push_back(temp->val);
-                temp = temp->next;
-            }
+        priority_queue<pair<int, ListNode*>,
+        vector<pair<int, ListNode*>>, greater<pair<int, ListNode*>>> pq;
+    
+    for(int i = 0; i < lists.size(); i++){
+        if(lists[i]){
+            pq.push({lists[i]->val, lists[i]});
         }
-
-        sort(nums.begin(), nums.end());
-
-
-        ListNode* dummy = new ListNode(0);
-        ListNode* curr = dummy;
-
-        for (int i = 0; i < nums.size(); i++) {
-            curr->next = new ListNode(nums[i]);
-            curr = curr->next;
-        }
-
-        return dummy->next;
     }
+    
+    ListNode* dummyNode = new ListNode(-1);
+    ListNode* temp = dummyNode;
+    
+    while(!pq.empty()){
+        auto it = pq.top();
+        pq.pop();
+        if(it.second->next){
+            pq.push({it.second->next->val, it.second->next});
+        }
+        
+        temp->next = it.second;
+        temp = temp->next;
+    }
+    
+    return dummyNode->next;
+}
 };
