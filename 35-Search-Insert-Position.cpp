2d7eef18b65851cbdc6a52f5734817fa48b87e21
Mayank Raj <144890239+MayankRaj435@users.noreply.github.com>
2026-07-18 11:32:09 +0530

    [35] [Time Beats: 100.00%] [Memory Beats: 43.31%] - LeetPush

--- 35-Search-Insert-Position.cpp
+++ 35-Search-Insert-Position.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        int ans=lower_bound(nums.begin(),nums.end(),target) - nums.begin();
+        int ans=lower_bound(nums.begin(),nums.end(),target) - nums.begin();  // same code for the upper bound
         return ans;
     }
 };
