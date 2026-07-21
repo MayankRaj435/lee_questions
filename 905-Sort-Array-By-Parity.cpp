2ce61269174092ea59357ac269b1e737a5e86c7a
Mayank Raj <144890239+MayankRaj435@users.noreply.github.com>
2026-07-21 17:29:08 +0530

    [905] [Time Beats: 100.00%] [Memory Beats: 80.75%] - LeetPush

--- 905-Sort-Array-By-Parity.cpp
+++ 905-Sort-Array-By-Parity.cpp
@@ -1,16 +1,23 @@
 class Solution {
 public:
     vector<int> sortArrayByParity(vector<int>& nums) {
-    vector<int> ans;
+    // vector<int> ans;                                  //first approach
 
-    for(int x : nums)
-        if(x % 2 == 0)
-            ans.push_back(x);
+    // for(int x : nums)
+    //     if(x % 2 == 0)
+    //         ans.push_back(x);
 
-    for(int x : nums)
-        if(x % 2 != 0)
-            ans.push_back(x);
+    // for(int x : nums)
+    //     if(x % 2 != 0)
+    //         ans.push_back(x);
 
-    return ans;
-        }
-    };
+    // return ans;
+    //     }
+    // };
+
+stable_partition(nums.begin(), nums.end(),[](int x){   // this will be used to put even first and then odd while preserving the order   and if one doesn't want to maintain preserve the order just remove "stable" from the method .
+        return x % 2 == 0;
+ });
+        return nums;
+}
+};
